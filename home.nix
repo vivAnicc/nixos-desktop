af38352f815cedf2f8353735830bc57f30700b25
nick <nicc.gemm@gmail.com>
2025-09-29 14:56:58 +0200

    Removed csharp module

--- home.nix
+++ home.nix
@@ -21,7 +21,6 @@
     modules/home-manager/vesktop.nix
     modules/home-manager/xdg.nix
     modules/home-manager/games.nix
-    modules/home-manager/csharp.nix
     # ./modules/home-manager/bitwarden.nix
   ];
 
