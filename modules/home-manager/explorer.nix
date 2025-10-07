76cb11d2dd1affd923f877dc39c0a6b5e7bb2e3e
nick <nicc.gemm@gmail.com>
2025-10-07 22:36:47 +0200

    Added explorer script

---
+++ modules/home-manager/explorer.nix
@@ -0,0 +1,38 @@
+{ pkgs, ... }:
+
+let
+  script = #fish
+  ''
+#!/usr/bin/env fish
+    if test (count $argv) != 1
+      echo "Incorrect usage" 1>&2
+      echo "Correct usage: $(status filename) <dir>" 1>&2
+      exit 1
+    end
+
+    set -l dir $argv[1]
+    set -l choosen (find -L $dir -nowarn -type d -maxdepth 5 -printf "%P\n" 2>/dev/null | wofi --matching=fuzzy --show dmenu)
+    printf $choosen
+  '';
+  package = pkgs.stdenvNoCC.mkDerivation {
+    name = "explorer";
+    version = "1.0";
+
+    src = builtins.toFile "explorer.fish" script;
+
+    buildInputs = [
+      pkgs.fish
+      pkgs.coreutils
+      pkgs.wofi
+    ];
+
+    dontUnpack = true;
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp $src $out/bin/explorer
+    '';
+  };
+in {
+  home.packages = [ package ];
+}
